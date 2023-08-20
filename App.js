d0201e07815a6339bf58f1c048da738c1431c3e0
Seok Yim(Noah) <seokyim8@gmail.com>
2023-08-19 21:32:14 -0400

    imported useState and created state for album_list

--- App.js
+++ App.js
@@ -1,7 +1,10 @@
 import { StyleSheet, TextInput, View, Button} from 'react-native';
+import { useState } from 'react';
 import Album from "./components/Add_album_container.js"
 
 export default function App() {
+  const [album_list, set_album_list] = useState([]);
+
   return (
     <View style={styles.app}>
       <Album />
