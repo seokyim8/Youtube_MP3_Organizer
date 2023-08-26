0e9807a3b44f30f2909b5ccff77899e41673f937
Seok Yim(Noah) <seokyim8@gmail.com>
2023-08-25 20:14:01 -0400

    Confirmed that expo av audio works perfectly fine as long as the provided URL is in mp3 form

--- components/Recording.tsx
+++ components/Recording.tsx
@@ -2,7 +2,7 @@ import { StyleSheet, View, Text } from 'react-native'
 import { useState, useEffect, ReactElement } from 'react'
 import Custom_button from './Custom_button'
 import { Recording as recording } from "../classes/Recording"
-// import Sound from 'react-native-sound'
+import { Audio } from 'expo-av';
 
 type Prop = {
     recording: recording
@@ -10,34 +10,34 @@ type Prop = {
 
 export default function Recording(props: Prop): ReactElement {
     const [is_playing, set_is_playing] = useState(false);
+    const [sound, setSound] = useState<any>();
 
-    function playPause(): void {
-        if(props.recording.sound === undefined){
-            console.log("Error: audio file does not exist for the selected recording");
-            return;
-        }
-        if(is_playing){
-            props.recording.sound.pause();
-            set_is_playing(false);
-        }
-        else{
-            set_is_playing(true);
-            props.recording.sound.play(success =>{
-                if(success){
-                    set_is_playing(false);
-                }
-                else{
-                    console.log("Error: audio playing has failed");
-                    set_is_playing(false);
-                }
-            });
-        }
+    async function playSound() {
+        console.log('Loading Sound');
+        await Audio.setAudioModeAsync({playsInSilentModeIOS: true});
+        console.log("Audio mode set");
+        const sound = new Audio.Sound();
+        await sound.loadAsync({
+            uri: "https://sample-music.netlify.app/death%20bed.mp3"
+        },
+        {shouldPlay: true});
+        console.log('Playing Sound');
+        await sound.playAsync();
     }
+
+    useEffect(() => {
+        // return sound
+        //   ? () => {
+        //       console.log('Unloading Sound');
+        //       sound.unloadAsync();
+        //     }
+        //   : undefined;
+      }, []);
 
     return (
         <View style={styles.container}>
             <Text style={styles.recording_name}>{props.recording.name}</Text>
-            <Custom_button text="Play Music" onPress={playPause} />
+            <Custom_button text="Play Music" onPress={playSound} />
         </View>
     );
 }
